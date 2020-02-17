682f2caee1c6df0fb1f638e68ddf92b6bbf097b6
Aditya Mishra <adityalmishra25@gmail.com>
2020-02-17 23:05:51 +0530

    registration basic layout ready

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,18 +1,81 @@
 <template>
-  <div>
-      <div id="reg">
-        <b-icon-people-fill style="width: 50px; height: 50px;"></b-icon-people-fill><b-form-input v-model="name" placeholder="Enter NGO's name"></b-form-input><br>
-        <b-form-input v-model="cause" placeholder="Cause"></b-form-input><br>
-        <b-form-input v-model="location" placeholder="Location"></b-form-input><br>
-        <b-form-input v-model="email" placeholder="Email"></b-form-input><br>
-        <b-form-input v-model="password" placeholder="Password"></b-form-input><br>
-        <b-form-input v-model="confiem_password" placeholder="Confirm Password"></b-form-input><br>
+  <div class="container-fluid">
+      <div id="reg" >
+        <b-card style="max-width: 80%; margin:7%">
+          <h1>Register</h1><h6>to Join us</h6>
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-people-fill style="width: 40px; height: 40px;"></b-icon-people-fill>
+              </b-col>
+              <b-col>
+                <b-form-input v-model="name" placeholder="Enter NGO's name"></b-form-input><br>
+              </b-col>
+            </b-row>
 
-        <div class="h2 mb-0">
-    <b-icon-arrow-up></b-icon-arrow-up>
-    <b-icon-alert-triangle-fill></b-icon-alert-triangle-fill>
-  </div>
-        <!-- <div class="mt-2">Value: {{ text }}</div> -->
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-people-fill style="width: 40px; height: 40px;"></b-icon-people-fill>
+              </b-col>
+              <b-col>
+                <b-form-input v-model="cause" placeholder="Cause"></b-form-input><br>
+              </b-col>
+            </b-row>
+            
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-geo style="width: 40px; height: 40px;"></b-icon-geo>
+              </b-col>
+              <b-col>
+            <b-form-input v-model="location" placeholder="Location"></b-form-input><br>
+              </b-col>
+            </b-row>
+
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-envelope-fill style="width: 40px; height: 40px;"></b-icon-envelope-fill>
+              </b-col>
+              <b-col>
+            <b-form-input v-model="email" placeholder="Email"></b-form-input><br>
+              </b-col>
+            </b-row>
+
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-people-fill style="width: 40px; height: 40px;"></b-icon-people-fill>
+              </b-col>
+              <b-col>
+            <b-form-input v-model="password" placeholder="Password"></b-form-input><br>
+              </b-col>
+            </b-row>
+
+            <b-row>
+              <b-col sm='1'>
+                <b-icon-people-fill style="width: 40px; height: 40px;"></b-icon-people-fill>
+              </b-col>
+              <b-col>
+            <b-form-input v-model="confirmpassword" placeholder="Confirm Password"></b-form-input><br>
+              </b-col>
+            </b-row>
+
+
+            <b-form-file
+              v-model="file"
+              :state="Boolean(file)"
+              placeholder="Choose a file or drop it here..."
+              drop-placeholder="Drop file here..."
+            ></b-form-file><br><br>
+
+            <b-button variant="outline-primary" style="align:left;">Submit</b-button>
+        </b-card>
+      
+      </div>
+      <div id="block2">
+        <div style="margin:10%; text-align:left">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            "Phasellus id ante eget lacus porta faucibus. Curabitur non placerat ligula. Proin ut metus tellus. Morbi non varius sapien. Pellentesque mollis tempus augue, sed porttitor velit egestas quis. Nunc porttitor, arcu ac viverra ultricies, urna diam sodales ipsum, ac semper ipsum sem ac felis. Fusce ac libero leo. Morbi lectus mi, rhoncus eu lorem vel, sollicitudin pharetra diam. Maecenas sapien elit, feugiat ac dolor id, pharetra mollis orci. Phasellus libero lacus, lobortis in velit ut, ornare ullamcorper nulla. Maecenas molestie ultricies nunc, vel consectetur mauris dignissim in. Etiam fringilla ipsum pharetra purus rhoncus, sed pharetra est sagittis. Nam sagittis, metus ac vestibulum accumsan, libero dui suscipit nibh, vitae maximus ipsum est sit amet ante. Suspendisse elementum molestie nisi, non convallis ligula volutpat ut. Vestibulum viverra pulvinar turpis, eu consequat tellus dapibus at.""
+          </p>
+        </div>
       </div>
   </div>
 </template>
@@ -22,14 +85,30 @@
   export default {
     data() {
       return {
-        text: ''
+        name:'',
+        cause:'',
+        location:'',
+        email:'',
+        confirmpassword:'',
+        file:''
       }
     }
   }
 </script>
 <style scoped>
     #reg{
-        margin-left: 100px;
-        margin-right: 100px;
+        
+        max-width: 50%;
+        background-color: darkgoldenrod;
+        flex:1;
+    }
+    #block2{
+      max-width: 50%;
+      background-color:black;
+      flex:1;
+    }
+    .container-fluid{
+      display: flex;
+
     }
 </style>
